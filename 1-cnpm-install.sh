b6aeebb9a9e42dac6b5015ed85be19a7de880a1d
jianan <1072772483@qq.com>
2019-03-20 23:45:27 +0800

    cnpm

---
+++ 1-cnpm-install.sh
@@ -0,0 +1,7 @@
+
+cd ./game-server && cnpm install -d --unsafe-perm && cnpm install mysql
+echo '============   game-server cnpm installed ============'
+cd ..
+cd ./web-server && cnpm install -d
+echo '============   web-server cnpm installed ============'
+
