27d77d4465b143e967f42e056e603867a1cd1253
jianan <1072772483@qq.com>
2019-09-06 22:37:08 +0800

    sql在每个服务器上都是独立的一份

--- game-server/app/dao/mysql/mysqlHelper.js
+++ game-server/app/dao/mysql/mysqlHelper.js
@@ -1,9 +1,3 @@
-
-/*
-对于callback的普适性：
-第一个参数是error，第二个参数是正确返回的数据
-一般情况下，两者仅有一个有效
-*/
 var mysqlHelper = function(app){
     var mysqlConfig = app.get('mysql');
     var mysql = require('mysql');
@@ -22,7 +16,6 @@ mysqlHelper.prototype.query = function(sqlString, values, callback){
     }
 };
 
-
 mysqlHelper.prototype.guestLogin = function(callback) {
 	var sql = "insert into t_user () values ();";
 	this.query(sql, null, function(err, results, fileds){
@@ -34,7 +27,6 @@ mysqlHelper.prototype.guestLogin = function(callback) {
         }
     }.bind(this));
 };
-
 
 mysqlHelper.prototype.queryUserInfo = function(userid, callback) {
     var sqlString = "select * from t_user where userid = '?'";
