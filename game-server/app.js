aa146b2a9f311bb55d0ed521990f5fe779282b66
jianan <1072772483@qq.com>
2019-09-06 22:19:35 +0800

    sql在每个服务器上都是独立的一份

--- game-server/app.js
+++ game-server/app.js
@@ -1,31 +1,33 @@
 var pomelo = require('pomelo');
 var app = pomelo.createApp();
-app.set('name', 'chatofpomelo-websocket');
+app.set('name', 'brnn-server');
 
 app.configure('production|development', 'connector', function () {
-	app.set('connectorConfig', {
-		connector: pomelo.connectors.hybridconnector,
-		heartbeat: 10,
-	});
+    app.set('connectorConfig', {
+        connector: pomelo.connectors.hybridconnector,
+        heartbeat: 10,
+    });
 });
 
 app.configure('production|development', 'gate', function () {
-	app.set('connectorConfig', {
-		connector: pomelo.connectors.hybridconnector, 
-		useProtobuf: true
-	});
+    app.set('connectorConfig', {
+        connector: pomelo.connectors.hybridconnector,
+        useProtobuf: true
+    });
 });
 
-app.loadConfig("mysql", app.getBase() + "/config/mysql.json"); // 初始化mysql配置
+// 初始化mysql配置
+app.loadConfig("mysql", app.getBase() + "/config/mysql.json");
 
-app.configure('production|development', function () { // 所有服务器都是mysql服务器的一个客户端
-	var Helper = require("./app/dao/mysql/mysqlHelper");
-	var sqlHelper = new Helper(app);
-	app.set("sqlHelper", sqlHelper);
+// 所有服务器都是mysql服务器的一个客户端
+app.configure('production|development', function () {
+    var Helper = require("./app/dao/mysql/mysqlHelper");
+    var sqlHelper = new Helper(app);
+    app.set("sqlHelper", sqlHelper);
 });
 
 app.start();
 
 process.on('uncaughtException', function (err) {
-	console.error(' Caught exception: ' + err.stack);
+    console.error(' Caught exception: ' + err.stack);
 });
